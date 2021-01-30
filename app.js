ebaad760eb45625637a88964679a2c98358fe8df
AniCloud <AnimeCloud@users.noreply.github.com>
2021-01-30 21:38:23 +0300

    Update 7.1: Framework7!

---
+++ app.js
@@ -0,0 +1,38 @@
+// Init App
+/*var app = new Framework7({
+  id: 'anicloud.ml',
+  root: '#app',
+  theme: theme,
+  debugger: false,
+  cache: false,
+  data: function () {
+    return {
+      user: {
+        firstName: 'Khelesh',
+        lastName: 'Mehra',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+	//closeByBackdropClick: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});*/
